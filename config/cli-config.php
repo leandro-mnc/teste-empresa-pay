f92e34cf24a98be3f3f8eb63eee8786cae367ebc
Leandro Teixeira <lteixeiramnc@yahoo.com>
2023-08-17 21:45:30 -0300

    Database Migration

--- config/cli-config.php
+++ config/cli-config.php
@@ -6,30 +6,43 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 use Slim\Factory\AppFactory;
-use Slim\Container;
 use DI\ContainerBuilder;
+use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\PhpFile;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Tools\Console\Command\DumpSchemaCommand;
+use Doctrine\Migrations\Tools\Console\Command\ExecuteCommand;
+use Doctrine\Migrations\Tools\Console\Command\GenerateCommand;
+use Doctrine\Migrations\Tools\Console\Command\LatestCommand;
+use Doctrine\Migrations\Tools\Console\Command\ListCommand;
+use Doctrine\Migrations\Tools\Console\Command\MigrateCommand;
+use Doctrine\Migrations\Tools\Console\Command\RollupCommand;
+use Doctrine\Migrations\Tools\Console\Command\StatusCommand;
+use Doctrine\Migrations\Tools\Console\Command\SyncMetadataCommand;
+use Doctrine\Migrations\Tools\Console\Command\VersionCommand;
 
 // Environment
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
 // Instantiate PHP-DI ContainerBuilder
 $containerBuilder = new ContainerBuilder();
 
 // Set up settings
-$appSettings = require __DIR__ . '/app/settings.php';
+$appSettings = require __DIR__ . '/../app/settings.php';
 $appSettings($containerBuilder);
 
 // Load Database
-$database = require __DIR__ . '/app/database.php';
+$database = require __DIR__ . '/../app/database.php';
 $database($containerBuilder);
 
 // Set up dependencies
-$dependencies = require __DIR__ . '/app/dependencies.php';
+$dependencies = require __DIR__ . '/../app/dependencies.php';
 $dependencies($containerBuilder);
 
 // Set up repositories
-$repositories = require __DIR__ . '/app/repositories.php';
+$repositories = require __DIR__ . '/../app/repositories.php';
 $repositories($containerBuilder);
 
 // Set up repositories
@@ -43,6 +56,30 @@ $container = $containerBuilder->build();
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-return ConsoleRunner::run(
-    new SingleManagerProvider($container->get(EntityManagerInterface::class))
+/** @var EntityManagerInterface $entityManager */
+$entityManager = $container->get(EntityManagerInterface::class);
+
+$migrations = new PhpFile(__DIR__ . '/../migrations.php');
+
+$dependencyFactory = DependencyFactory::fromEntityManager(
+    $migrations,
+    new ExistingEntityManager($entityManager)
+);
+
+$commands = [
+    new DumpSchemaCommand($dependencyFactory),
+    new ExecuteCommand($dependencyFactory),
+    new GenerateCommand($dependencyFactory),
+    new LatestCommand($dependencyFactory),
+    new ListCommand($dependencyFactory),
+    new MigrateCommand($dependencyFactory),
+    new RollupCommand($dependencyFactory),
+    new StatusCommand($dependencyFactory),
+    new SyncMetadataCommand($dependencyFactory),
+    new VersionCommand($dependencyFactory)
+];
+
+ConsoleRunner::run(
+    new SingleManagerProvider($entityManager),
+    $commands
 );
